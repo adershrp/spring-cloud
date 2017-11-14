98304c9ea30932f2131403876b95400762433a4f
Adersh.P <Adersh.P@LTPBAN182340746.corp.mphasis.com>
2017-11-14 10:00:29 +0530

    Initial

---
+++ pcf-service-02/src/main/java/com/pcf/study/PcfService02Application.java
@@ -0,0 +1,12 @@
+package com.pcf.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfService02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfService02Application.class, args);
+	}
+}
